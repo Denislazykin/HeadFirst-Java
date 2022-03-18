372cb2149e99a7b8037d7a68abb405532c14319f
Denis Lazykin <dlazykin856@gmail.com>
2022-03-18 18:13:51 +0300

    add class PoorDog

---
+++ src/main/java/com/HeadFirst/chapter_4/PoorDog/PoorDog.java
@@ -0,0 +1,22 @@
+package main.java.com.HeadFirst.chapter_4.PoorDog;
+
+public class PoorDog {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
