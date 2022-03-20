0817906b0248534955c4174700515414de8db4f9
Denis Lazykin <dlazykin856@gmail.com>
2022-03-20 11:02:49 +0300

    edit GuessGame

--- src/main/java/com/HeadFirst/chapter_2/GuessGame/GuessGame.java
+++ src/main/java/com/HeadFirst/chapter_2/GuessGame/GuessGame.java
@@ -1,62 +1,62 @@
 package main.java.com.HeadFirst.chapter_2.GuessGame;
 
 public class GuessGame {
-/*
-Добавим три переменных экземпляра
-для трех объектов Player
- */
+    /*
+    Добавим три переменных экземпляра
+    для трех объектов Player
+     */
     Player p1;
     Player p2;
     Player p3;
 
     public void startGame() {
-/*
-Создаем три объекта
- */
+        /*
+        Создаем три объекта
+         */
         p1 = new Player();
         p2 = new Player();
         p3 = new Player();
-/*
-Объявляем три переменнве для хранения
-вариантов от каждого игрока
- */
+        /*
+        Объявляем три переменнве для хранения
+        вариантов от каждого игрока
+         */
         int guessp1;
         int guessp2;
         int guessp3;
-/*
-Переменные для хранения правельности
-или не правильности ответов
- */
+        /*
+        Переменные для хранения правельности
+        или не правильности ответов
+         */
         boolean p1isRight = false;
         boolean p2isRight = false;
         boolean p3isRight = false;
-/*
-Создаем число которое должны угадать
- */
+        /*
+        Создаем число которое должны угадать
+         */
         int targetNumber = (int) (Math.random() * 10);
         System.out.println("Я загадываю число от 0 до 9...");
 
         while (true) {
             System.out.println("Число, которое нужно угадать, - " + targetNumber);
-/*
-Вызываем метод guess
- */
+            /*
+            Вызываем метод guess
+             */
             p1.guess();
             p2.guess();
             p3.guess();
-/*
-Извлекаем варианты каждого игрока
-получая доступ к их переменным number
- */
+            /*
+            Извлекаем варианты каждого игрока
+            получая доступ к их переменным number
+             */
             guessp1 = p1.number;
             System.out.println("Первый игрок думает, что это " + guessp1);
             guessp2 = p2.number;
             System.out.println("Второй игрок думает, что это " + guessp2);
             guessp3 = p3.number;
             System.out.println("Третий игрок думает, что это " + guessp3);
-/*
-Проверяем варианты игроков
- */
+            /*
+            Проверяем варианты игроков
+             */
             if (guessp1 == targetNumber) {
                 p1isRight = true;
             }
