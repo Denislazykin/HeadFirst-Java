39fafe93a94813cda944586c7154f753aad64198
Denis Lazykin <dlazykin856@gmail.com>
2022-03-22 13:39:25 +0300

    Поработайте компилятором Monsters

---
+++ src/main/java/com/HeadFirst/chapter_7/Monsters/Dragon.java
@@ -0,0 +1,8 @@
+package main.java.com.HeadFirst.chapter_7.Monsters;
+
+public class Dragon extends Monster {
+    boolean frighten(int degree) {
+        System.out.println("Огненное дыхание");
+        return true;
+    }
+}
---
+++ src/main/java/com/HeadFirst/chapter_7/Monsters/Vampire.java
@@ -0,0 +1,8 @@
+package main.java.com.HeadFirst.chapter_7.Monsters;
+
+public class Vampire extends Monster {
+    boolean frighten(int x) {
+        System.out.println("Укусить?");
+        return false;
+    }
+}
