bebb0f741cfc1e365562a6e98f809e2b1c897192
Denis Lazykin <dlazykin856@gmail.com>
2022-03-24 17:59:24 +0300

    add TestHippo class

---
+++ src/main/java/com/HeadFirst/chapter_9/Animals/TestHippo.java
@@ -0,0 +1,8 @@
+package main.java.com.HeadFirst.chapter_9.Animals;
+
+public class TestHippo {
+    public static void main(String[] args) {
+        System.out.println("To begin...");
+        Hippo h = new Hippo();
+    }
+}
