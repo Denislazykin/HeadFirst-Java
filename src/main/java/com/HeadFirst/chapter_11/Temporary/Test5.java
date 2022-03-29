63f708fad1b0b285e898139a87af08bd2967a8b8
Denis Lazykin <dlazykin856@gmail.com>
2022-03-29 18:11:56 +0300

    add Test5

---
+++ src/main/java/com/HeadFirst/chapter_11/Temporary/Test5.java
@@ -0,0 +1,20 @@
+package main.java.com.HeadFirst.chapter_11.Temporary;
+
+import java.io.*;
+
+public class Test5 {
+    public static void main(String[] args) {
+        File f = new File("test.txt");
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            System.out.println("Have a good day");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("We throw exception :" + e);
+        }
+        finally {
+            System.out.println("This is a finaly block");
+        }
+        System.out.println("Etot kod ne imeet otnosheniya k isklucheniam ");
+    }
+}
