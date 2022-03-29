9363cbbf0d1c3bb7d913d7109516f9675c71db4a
Denis Lazykin <dlazykin856@gmail.com>
2022-03-29 18:09:57 +0300

    Наточите свой карандаш exception

---
+++ src/main/java/com/HeadFirst/chapter_11/Temporary/TestException.java
@@ -0,0 +1,27 @@
+package main.java.com.HeadFirst.chapter_11.Temporary;
+
+public class TestException {
+    public static void main(String[] args) {
+//        String test = "no";
+        String test = "yes";
+        try {
+            System.out.println("Начало try");
+            doRisky(test);
+            System.out.println("Конец try");
+        } catch (Exception se){
+            System.out.println("--- Жуткое исключение ---");
+        } finally {
+            System.out.println("finally");
+        }
+        System.out.println("Конец main");
+    }
+
+    static void doRisky(String test) throws Exception{
+        System.out.println("Начало опасного метода");
+        if ("yes".equals(test)) {
+            throw new Exception();
+        }
+        System.out.println("Конец опасного метода");
+        //return;
+    }
+}
