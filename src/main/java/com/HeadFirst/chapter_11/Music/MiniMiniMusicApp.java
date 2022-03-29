a15659dff115e8d52074abb2c6f74f73f6c7273f
Denis Lazykin <dlazykin856@gmail.com>
2022-03-29 18:34:34 +0300

    edit MiniMiniMusicApp class

--- src/main/java/com/HeadFirst/chapter_11/Music/MiniMiniMusicApp.java
+++ src/main/java/com/HeadFirst/chapter_11/Music/MiniMiniMusicApp.java
@@ -1,4 +1,38 @@
 package main.java.com.HeadFirst.chapter_11.Music;
 
+import javax.sound.midi.*;
+
 public class MiniMiniMusicApp {
-}
+    public static void main(String[] args) {
+        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+        mini.play();
+    }//  закрываем main
+
+    public void play() {
+        try {
+            Sequencer player = MidiSystem.getSequencer();   //получаем синтезатор
+            player.open(); // открываем его, чтобы начать использовать
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4); // создадим последовательность
+
+            Track track = seq.createTrack(); // заправшиваем трек у последовательности
+            // трек находится внутри последовательности, а MIDI-данные - в треке
+
+            ShortMessage a = new ShortMessage();    // помещаем в трек MIDI-события
+            a.setMessage(144, 1, 44, 100);
+            MidiEvent noteOne = new MidiEvent(a, 1);
+            track.add(noteOne);
+
+            ShortMessage b = new ShortMessage();    // помещаем в трек MIDI-события
+            b.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+
+            player.setSequence(seq); // передаем последовательность синтезатору
+
+            player.start(); // запускаем синтезатор
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    } // закрываем play
+} // закрываем класс
