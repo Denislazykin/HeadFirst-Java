7fd585730db95db17fac2419ea24453750a9300f
Denis Lazykin <dlazykin856@gmail.com>
2022-03-30 19:01:27 +0300

    add MyDrawPanel

---
+++ src/main/java/com/HeadFirst/chapter_12/DrawPanel/MyDrawPanel.java
@@ -0,0 +1,12 @@
+package main.java.com.HeadFirst.chapter_12.DrawPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collections;
+
+public class MyDrawPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.green);
+        g.fillRect(20,50,100,100);
+    }
+}
