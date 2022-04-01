1bc2779b1e771d543579dbc21149e3b9ab5901c2
Denis Lazykin <dlazykin856@gmail.com>
2022-04-01 16:32:06 +0300

    edit MakeHippo

--- src/main/java/com/HeadFirst/chapter_9/FamilyAnimals/MakeHippo.java
+++ src/main/java/com/HeadFirst/chapter_9/FamilyAnimals/MakeHippo.java
@@ -2,7 +2,7 @@ package main.java.com.HeadFirst.chapter_9.FamilyAnimals;
 
 public class MakeHippo {
     public static void main(String[] args) {
-        Hippo hippo = new Hippo("Buffu");
+        Hippo hippo = new Hippo("Buffy");
         System.out.println(hippo.getName());
     }
 }
