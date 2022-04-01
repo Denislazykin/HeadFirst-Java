1d9ad37b7bce05aca3a883a5d0af09a5d4a45679
Denis Lazykin <dlazykin856@gmail.com>
2022-04-01 16:30:51 +0300

    edit Monster

--- src/main/java/com/HeadFirst/chapter_7/Monsters/Monster.java
+++ src/main/java/com/HeadFirst/chapter_7/Monsters/Monster.java
@@ -2,7 +2,7 @@ package main.java.com.HeadFirst.chapter_7.Monsters;
 
 public class Monster {
     boolean frighten(int d) {
-        System.out.println("ГрррРРРррРРРР");
+        System.out.println("ГррРРрРРР");
         return true;
     }
 }
