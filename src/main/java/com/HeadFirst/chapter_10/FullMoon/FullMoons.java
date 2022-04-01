5c74fdf53dc045d8d509ba6a381ef7abcb209f74
Denis Lazykin <dlazykin856@gmail.com>
2022-04-01 16:33:50 +0300

    edit FullMoons

--- src/main/java/com/HeadFirst/chapter_10/FullMoon/FullMoons.java
+++ src/main/java/com/HeadFirst/chapter_10/FullMoon/FullMoons.java
@@ -3,12 +3,24 @@ package main.java.com.HeadFirst.chapter_10.FullMoon;
 import java.util.Calendar;
 
 public class FullMoons {
-    static int DAY_IM = 1000 * 60 *60 * 24; //количество милисекунд в сутках
+    /*
+     * количество милисекунд в сутках
+     */
+    static int DAY_IM = 1000 * 60 *60 * 24;
 
     public static void main(String[] args) {
-        Calendar c = Calendar.getInstance(); //получим экземпляр календаря
-        c.set(2020,0,1,15,40); // установим значение
-        long day1 = c.getTimeInMillis(); // получим его в милисекундах
+        /*
+         * получим экземпляр календаря
+         */
+        Calendar c = Calendar.getInstance();
+        /*
+         * установим значение
+         */
+        c.set(2020,0,1,15,40);
+        /*
+         * получим его в милисекундах
+         */
+        long day1 = c.getTimeInMillis();
         for (int x = 0; x < 60; x++) {
             day1 += (DAY_IM * 29.52);
             c.setTimeInMillis(day1);
