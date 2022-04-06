bb067d3f27643324c9bcc733f76a55c30c52176c
Denis Lazykin <dlazykin856@gmail.com>
2022-04-06 18:16:20 +0300

    add socket MyRunnable

---
+++ src/main/java/com/HeadFirst/chapter_15/Run/MyRunnable.java
@@ -0,0 +1,24 @@
+package main.java.com.HeadFirst.chapter_15.Run;
+
+public class MyRunnable implements Runnable{
+    public void run() {
+        go();
+    }
+
+    public void go() {
+        doMore();
+    }
+
+    public void doMore() {
+        System.out.println("Вершина стека");
+    }
+}
+
+class ThreadTester {
+    public static void main(String[] args) {
+        Runnable threadJob = new MyRunnable();
+        Thread myThread = new Thread(threadJob);
+        myThread.start();
+        System.out.println("Возвращаемся в main");
+    }
+}
