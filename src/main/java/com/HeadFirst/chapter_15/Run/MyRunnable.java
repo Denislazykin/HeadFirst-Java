07c30a9f5685b626ac322bf6d749bc328f462380
Denis Lazykin <dlazykin856@gmail.com>
2022-04-07 15:09:34 +0300

    edit class MyRunnable

--- src/main/java/com/HeadFirst/chapter_15/Run/MyRunnable.java
+++ src/main/java/com/HeadFirst/chapter_15/Run/MyRunnable.java
@@ -8,8 +8,7 @@ public class MyRunnable implements Runnable {
     public void go() {
         try {
             Thread.sleep(2000);
-        }
-        catch (InterruptedException ex) {
+        } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
         doMore();
