25ef80bbfd53c14afd0089c0cc94f16993e3ba21
Denis Lazykin <dlazykin856@gmail.com>
2022-04-08 18:29:57 +0300

    add Jukebox8

---
+++ src/main/java/com/HeadFirst/chapter_16/Jukebox8/Song.java
@@ -0,0 +1,57 @@
+package main.java.com.HeadFirst.chapter_16.Jukebox8;
+
+import java.util.Objects;
+
+class Song implements Comparable<Song> {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    public Song(String title, String artist, String rating, String bpm) {
+        this.title = title;
+        this.artist = artist;
+        this.rating = rating;
+        this.bpm = bpm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(title, song.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public int compareTo(Song o) {
+        return title.compareTo(o.getTitle());
+    }
+}
+
